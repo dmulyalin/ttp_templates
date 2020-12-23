301259ff7af4fcb315f0e18354ad4f45e5a26f85
Denis <d.mulyalin@gmail.com>
2020-12-23 19:03:48 +1100

    fixed sdist to include template files in PyPi distro

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ __author__ = "Denis Mulyalin <d.mulyalin@gmail.com>"
 
 setup(
     name="ttp_templates",
-    version="0.1.0",
+    version="0.1.1",
     author="Denis Mulyalin",
     author_email="d.mulyalin@gmail.com",
     description="Template Text Parser Templates collections",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/dmulyalin/ttp_templates",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     extras_require={},
     include_package_data=True,
     package_data={
