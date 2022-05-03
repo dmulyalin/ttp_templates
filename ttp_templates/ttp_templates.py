6f0a77d64818f4f133fa4b094a9d791592d0928b
Denis <d.mulyalin@gmail.com>
2022-05-03 22:53:18 +1000

    fixing __file__ issue for ubuntu

--- ttp_templates/ttp_templates.py
+++ ttp_templates/ttp_templates.py
@@ -53,18 +53,11 @@ def get_template(
         return None
 
     template_dir = os.path.abspath(os.path.dirname(__file__))
-    print("!!!!!!!!!!")
-    print("path: ", path)
-    print("template_dir: ", template_dir)
-    print("__file__: ", __file__)
-    print("os.path.isdir(template_dir): ", os.path.isdir(template_dir))
-    print('template_dir.count("ttp_templates"): ', template_dir.count("ttp_templates"))
-    print("os.path.exists(os.path.join(template_dir, path)): ", os.path.exists(os.path.join(template_dir, path)))
 
     # Ubuntu returns path that looks like this when calling run_ttp for Netmiko:
     # '/home/runner/work/ttp_templates/ttp_templates/ttp_templates'
-    # below code is to ensure we go two levels up
-    if not os.path.isdir(template_dir) and template_dir.count("ttp_templates") == 3:
+    # below code is to ensure we go two levels up and fix FileNotFoundError
+    if template_dir.count("/ttp_templates") == 3:
         template_dir = os.path.dirname(os.path.dirname(template_dir))
         
     template_filename = os.path.join(template_dir, path)
